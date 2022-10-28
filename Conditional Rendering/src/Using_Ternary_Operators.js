924c76164a67a1d447d66f1c86b11e205fe7d89a
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2022-10-28 17:55:31 +0530

    Create Using_Ternary_Operators.js

---
+++ Conditional Rendering/src/Using_Ternary_Operators.js
@@ -0,0 +1,16 @@
+import { Component } from "react"
+import './App.css'
+
+class App extends Component {
+
+  render() {
+    const { isLoggedIn } = this.state
+    return (
+      <div className="container">
+        {isLoggedIn ? <button>Logout</button> : <button>Login</button>}
+      </div>
+    )
+  }
+}
+
+export default App
