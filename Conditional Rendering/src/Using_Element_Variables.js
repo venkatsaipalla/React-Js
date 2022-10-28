4e5d635895ed6257e567aa6b9c7fba3a587c5662
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2022-10-28 17:55:05 +0530

    Create Using_Element_Variables.js

---
+++ Conditional Rendering/src/Using_Element_Variables.js
@@ -0,0 +1,24 @@
+import { Component } from "react"
+import './App.css'
+
+class App extends Component {
+  state = { isLoggedIn: true }
+
+  render() {
+    const { isLoggedIn } = this.state
+    let authButton
+    if (isLoggedIn) {
+      authButton = <button>Logout</button>
+    } else {
+      authButton = <button>Login</button>
+    }
+    return (
+      <div className="container">
+        <h1>React JS</h1>
+        {authButton}
+      </div>
+    )
+ }
+}
+
+export default App
