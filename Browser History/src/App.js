5477d868e51753bc558f4e370d58d40f68baab52
Palla Venkata Sai <72677771+venkatsaipalla@users.noreply.github.com>
2022-12-12 00:00:29 +0530

    Delete Browser History/src directory

--- Browser History/src/App.js
+++
@@ -1,8 +0,0 @@
-import './App.css'
-import History from './components/History/index'
-// These are the list used in the application. You can move them to any component needed.
-
-// Replace your code here
-const App = () => <History />
-
-export default App
